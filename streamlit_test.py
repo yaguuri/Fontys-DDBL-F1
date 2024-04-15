1fef43c26c4efd515a6bfefd1d1eabf3a2749f7f
LeeYeseul <lys1710@naver.com>
2024-04-15 11:45:53 +0200

    add file

---
+++ streamlit_test.py
@@ -0,0 +1,34 @@
+import streamlit as st
+
+col1, col2 = st.columns([2,3]) #split space into 2:3
+
+#contents of col1
+with col1:
+    st.title("column1")
+
+#contents of col2
+with col2:
+    st.title("column2")
+    st.checkbox("checkbox1 in col2")
+    st.checkbox("checkbox2 in col2")
+
+
+#Tab
+tab1, tab2 = st.tabs(["Tab A", "Tab B"])
+
+with tab1:
+    st.write("Here is the Tab 1")
+
+with tab2:
+    st.write("Here is the Tab 2")
+
+
+#Side Bar
+st.sidebar("SideBar1")
+
+#Put Image
+from PIL import Image
+
+logo_img = Image.open("Desktop/DEEPSEARCH logo.png")
+
+
