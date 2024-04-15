1a9a70bdd0ae03fdf0a250d811436221bebd4d81
LeeYeseul <lys1710@naver.com>
2024-04-15 12:56:17 +0200

    edit

--- streamlit_test.py
+++ streamlit_test.py
@@ -7,35 +7,15 @@ import streamlit as st
 #from PIL import Image
 #logo_img = Image.open("C:/Users/lys17/Desktop/DEEPSEARCH_logo.png")
 
-#Columns
-col1, col2 = st.columns([2,3]) #split space into 2:3
+#Extract Dataframe
+st.title("column1")
 
-#contents of col1
-with col1:
-    st.title("column1")
+#Initialize connection
+conn = st.connection("mysql", type="sql")
 
-    #Initialize connection
-    conn = st.connection("mysql", type="sql")
-
-    #perform query
-    #ttl=600: to ensure the query result is cached for no longer than 10 minutes
-    df = conn.query("SELECT * FROM player;", ttl=600)
+#perform query
+#ttl=600: to ensure the query result is cached for no longer than 10 minutes
+df = conn.query("SELECT * FROM player;", ttl=600)
     
-    #print result
-    st.table(df.head(10))
-
-#contents of col2
-with col2:
-    st.title("column2")
-    st.checkbox("checkbox1 in col2")
-    st.checkbox("checkbox2 in col2")
-
-
-#Tab
-tab1, tab2 = st.tabs(["Tab A", "Tab B"])
-
-with tab1:
-    st.write("Here is the Tab 1")
-
-with tab2:
-    st.write("Here is the Tab 2")
+#print result
+st.table(df.head(10))
